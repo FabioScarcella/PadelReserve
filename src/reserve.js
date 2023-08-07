a4ae522c20b42daed819f96259840120a11837a1
Fabio Scarcella <fabio.scarcella.gomez@gmail.com>
2023-08-07 16:45:01 +0200

    core: select hour, date and court

--- src/reserve.js
+++ src/reserve.js
@@ -6,11 +6,11 @@ export class Reserve {
 
     this.date = process.env.DATE;
     this.hour = process.env.HOUR;
+    this.preferedCourt = process.env.PREFEREDCOURT - 1;
+    this.time = process.env.TIME - 1;
   }
 
   async scrollUntilVisible(targetElement, container) {
-    const scrollDistance = 760;
-
     const childSelector = `[class*="bkc-pistas-disponibles"]`;
     const containerSelector = `[id="div-hores-disponibles-8"]`;
 
@@ -29,7 +29,6 @@ export class Reserve {
           let lastChildBoundingBox = null;
 
           for (let i = 0; i < children.length; i++) {
-            debugger;
             const child = children[i];
             const rect = child.getBoundingClientRect();
 
@@ -88,32 +87,114 @@ export class Reserve {
     await this.page.click('[data-id="r-8"]');
   }
 
+  async checkIfHourIsVisible(hour) {
+    return new Promise((resolve, reject) => {
+      if (hour.isVisible()) resolve();
+      else reject();
+    });
+  }
+
+  delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async scrollDown(px) {
+    this.page.evaluate((px) => {
+      window.scrollBy(0, px); // Scroll down by 500 pixels
+    }, px);
+  }
+
   async selectDateAndHour() {
     // *= MEANS THAT WILL FIND THE ELEMENT THAT CONTAINS THE SPECIFIED SUBSTRING.
     const date = await this.page.$(`[aria-label*="${this.date}"]`);
-    if (date) date.click();
+    await this.delay(500);
+    if (date) await date.click();
 
     // CHECK IF SELECTOR IS VISIBLE FIRST
-    const selector = `[data-hora="this.hour"]`;
+    const selector = `[data-hora="${this.hour}"]`;
     let hour = await this.page.$(selector);
-    if (!hour) {
+    let visible = await this.checkIfHourIsVisible(hour)
+      .then(() => true)
+      .catch(() => false);
+
+    while (!visible) {
       const element = await this.page.$('[class*="pistas-hora-more"]');
+      await this.page.waitForSelector("#div-hores-disponibles-8");
       const childDiv = await this.page.$(`#div-hores-disponibles-8`);
       const parentElement = await childDiv.$x("..");
       await this.scrollUntilVisible(element, parentElement[0]);
-      //element.click();
 
       hour = await this.page.$(selector);
+      visible = await this.checkIfHourIsVisible(hour)
+        .then(() => true)
+        .catch(() => false);
+
+      await this.delay(1500);
+    }
+    const containsClass = await hour.evaluate((element) => {
+      return element.classList.contains("pistas-hora-ok"); // Replace with the class name to check
+    });
+
+    if (containsClass) await hour.click();
+    else {
+      console.log("Hour full");
+      this.browser.close();
+    }
+  }
+
+  async selectCourt() {
+    const containerSelector = `[id="form-listado-pistas-8"]`;
+    const courtExpanded = `[aria-expanded="true"]`;
+    await this.page.waitForSelector(courtExpanded);
+
+    this.scrollDown(500);
+
+    await this.delay(100);
+    let childrens = await this.page.evaluate((containerSelector) => {
+      const element = document.querySelector(containerSelector);
+      if (element)
+        return [
+          ...Array.from(element.children)
+            .filter((child) => child.tagName === "DIV")
+            .map((child) => child.id),
+        ];
+      return [];
+    }, containerSelector);
+    if (!childrens) {
+      console.log("UNDEFINED COURTS");
+      return;
     }
 
-    //    hour.click();
+    //CHECK FOR PREFERED
+    const courtSelector = `#${childrens[this.preferedCourt]}`;
+    const court = await this.page.$(courtSelector);
+    await court.click();
+    await this.delay(500);
+    await this.scrollDown(500);
+
+    let courtHours = await this.page.evaluate((courtSelector) => {
+      const element = document.querySelector(courtSelector);
+      if (element)
+        return [
+          ...Array.from(
+            element.children[1].children[0].children[0].children[0].children
+          )
+            .filter((child) => child.tagName === "LI")
+            .map((child) => child.id),
+        ];
+      return [];
+    }, courtSelector);
+    const preferedHourSelector = `#${courtHours[this.time]}`;
+    const preferedHour = await this.page.$(preferedHourSelector);
+
+    preferedHour.click();
   }
 
   async main() {
     const p = await this.browser.pages();
     this.page = p[1];
-
     await this.getToBooking();
     await this.selectDateAndHour();
+    await this.selectCourt();
   }
 }
